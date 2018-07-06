f2fe51ec29889f371e81b546e516b154ac980c23
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-07-06 03:10:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function calculatesFarePrice (a , b) {
   if (distance <= 400) {
    return 0;
  } else if (distance > 400 && distance <= 2000) {
-   return .02 * distance;
+   return distance;
  } else if (distance > 2000 && distance < 2500) {
    return 25;
  } else {
